3eba488672fcca7aa95c6b742ceb2174eebae432
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-03-18 15:35:35 +0200

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/pizzahouse',
+  assetPrefix: '/pizzahouse',
 };
 
 export default nextConfig;
